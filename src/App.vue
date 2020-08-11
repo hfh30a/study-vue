415e0757933e0c642fc081b2c5a325fda40a4f21
hfh30a <ddr.dydy.0557@gmail.com>
2020-08-11 16:20:34 +0900

    format documents

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
     <InputField />
+    <SearchField />
   </div>
 </template>
 
 <script>
 import InputField from "./component/InputField";
+import SearchField from "./component/SearchField";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App",
+    };
   },
-  components: { InputField }
-}
+  components: { InputField, SearchField },
+};
 </script>
 
 <style>
