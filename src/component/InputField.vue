c9e7c8c39d7be947ecc98ac9552c066188485a57
hfh30a <ddr.dydy.0557@gmail.com>
2020-08-12 16:34:26 +0900

    名前フィールドの警告を文字数によって行う

--- src/component/InputField.vue
+++ src/component/InputField.vue
@@ -2,13 +2,15 @@
   <div>
     <div>
       <label>名前 :</label>
-      <input type="text" id="name" name="name" />
-      <span class="required">(※)</span>
+      <input type="text" id="name" name="name" v-model="name" />
+      <span class="required" v-if="isIdRequired">{{ required }}</span>
+      <span class="ok" v-else>OK!</span>
+      <span class="display-none">{{ nameCount }}</span>
     </div>
     <div>
       <label>年齢 :</label>
       <input type="text" id="age" name="age" />
-      <span class="required">(※)</span>
+      <span class="required" v-if="isAgeRequired">{{ required }}</span>
     </div>
     <div>
       <label>備考 :</label>
@@ -25,10 +27,36 @@
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      name:"",
+      isIdRequired: true,
+      isAgeRequired: false,
+      required: "必須項目です"
+    };
+  },
+  computed: {
+   nameCount(){
+     console.log(this.name.length)
+     if(this.name.length === 0) {
+       this.isIdRequired = true
+     } else {
+       this.isIdRequired = false
+     }
+     return this.name.length
+   }
+  },
+};
+</script>
 
 <style>
 .required {
   color: red;
+}
+
+.ok {
+  color: green;
 }
 </style>
