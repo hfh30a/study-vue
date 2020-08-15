2c78b8b99f4157023c7643de108ce929aa471b81
hfh30a <ddr.dydy.0557@gmail.com>
2020-08-16 00:35:10 +0900

    style の読み込み箇所を変更

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css' // add
-import 'bootstrap-vue/dist/bootstrap-vue.css' // add
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '../src/style/main.css'
 Vue.config.productionTip = false
 
 new Vue({
